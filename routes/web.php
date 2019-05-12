b7ef0bf77aaef1132bcc760cf4945f124ef62f6e
yan8909 <yan8909@gmail.com>
2019-05-12 06:41:32 +0000

    view - products list

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,5 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
 });
 
+Route::redirect('/', '/products')->name('root');
+Route::get('products', 'ProductsController@index')->name('products.index');
