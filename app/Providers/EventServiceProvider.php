90e6de181442f5ed397b0426824325e4c54bc7cf
yan8909 <yan8909@gmail.com>
2019-06-08 02:19:15 +0800

    product review

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -9,6 +9,8 @@ use App\Listeners\UpdateProductSoldCount;
 use App\Listeners\SendOrderPaidMail;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use App\Events\OrderReviewed;
+use App\Listeners\UpdateProductRating;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -24,6 +26,9 @@ class EventServiceProvider extends ServiceProvider
         OrderPaid::class => [
             UpdateProductSoldCount::class,
             SendOrderPaidMail::class,
+        ],
+        OrderReviewed::class => [
+            UpdateProductRating::class,
         ],
     ];
 
